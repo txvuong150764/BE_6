0b3717b07fa9f0002f357c71b24604997113f306
vuongtran <txvuong150764@gmail.com>
2024-04-28 00:24:39 +1000

    Fixed remove

--- Week_9_Sat_Algo/src/LongestCommonPrefix.java
+++ Week_9_Sat_Algo/src/LongestCommonPrefix.java
@@ -1,2 +1,17 @@
 public class LongestCommonPrefix {
+    public static String longestCommonPrefix(String[] strs) {
+        for(int i = 0; i < strs[0].length(); i++) {
+            for(String s : strs) {
+                if(i >= s.length() || s.charAt(i) != strs[0].charAt(i))
+                    return strs[0].substring(0, i);
+            }
+        }
+
+        return strs[0];
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flight", "flow"};
+        System.out.println(longestCommonPrefix(strs));
+    }
 }
