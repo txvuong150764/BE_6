9b4099ace2da906f4cba48938e87ccea74945a15
vuongtran <txvuong150764@gmail.com>
2024-05-08 02:06:21 +1000

    Fixed OptimizingFarthestDifferent

--- Week_11_Algo/src/OptimizingFarthestDifferent.java
+++ Week_11_Algo/src/OptimizingFarthestDifferent.java
@@ -1,7 +1,8 @@
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class OptimizingFarthestDifferent {
-    public static int solution(int[] a) {
+    public static int firstSolution(int[] a) {
         int ans = -1;
         HashMap<Integer, int[]> pos = new HashMap<>();
 
@@ -9,25 +10,33 @@ public class OptimizingFarthestDifferent {
             if(!pos.containsKey(a[i])) {
                 pos.put(a[i], new int[] {i, 1});
             }
-            else {
+            else if(a[i - 1] != a[i]){
                 ans = Math.max(ans, i - pos.get(a[i])[0] - 1);
-                pos.put(a[i], new int[] {i,  pos.get(a[i])[1] + 1});
+                pos.put(a[i], new int[]{pos.get(a[i])[0], pos.get(a[i])[1] + 1});
             }
         }
-
         for(int key : pos.keySet()) {
             if(pos.get(key)[1] == 1) {
-                ans = Math.max(ans, a.length - pos.get(key)[1]);
+                ans = Math.max(ans, a.length - pos.get(key)[0]);
             }
         }
 
         return ans;
     }
-    public static void main(String[] args) {
-        int[] a = new int[75001];
-        for(int i = 0; i < a.length; i++) {
-            a[i] = i;
+
+    public static int secondSolution(int[] a) {
+        int ans = 0;
+
+        for (int i = 0; i < a.length; ++i) {
+            if (a[i] != a[a.length - 1] || a[0] != a[a.length - 1 - i]) {
+                ans = a.length - i - 1;
+                return ans;
+            }
         }
-        System.out.println(solution(a));
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] a = {4, 6, 2, 2, 6, 6, 4};
+        System.out.println(firstSolution(a));
     }
 }
